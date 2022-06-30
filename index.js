7d360608b94c679513c0a51006bbf46aa9e3574b
Ken Ingle <ken@keningle.net>
2022-06-30 11:20:20 -0600

    Add fake/simple auth, add todos API

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const Router = require('koa-router');  // Router for Koa
 const fs = require("fs");              // Normal Node.js filesystem
 const fsp = require("fs").promises;    // filesystem with better async
 const path = require('node:path');     // for getting directory paths
+const { v4: uuidv4 } = require('uuid');
+const { request } = require('http');
 
+const AUTH_ENABLED = false;
 const PORT = 3000;  // default port we'll listen on
 const STORAGE_PATH = path.join(__dirname, 'storage');
 const APP_NAME = "STORAGE-API";  // just used for logging
@@ -13,6 +16,164 @@ const APP_NAME = "STORAGE-API";  // just used for logging
 // Instantiate our new app and router
 const app = new Koa();
 const router = new Router();
+
+let loggedInUsers = [];
+
+// -----------------------------------------------------
+function addLoggedInUser(userName) {
+  const user = loggedInUsers.filter(u => u.user !== userName);
+  if (!user || user.length === 0) {
+    const auth_token = `${uuidv4()}-${uuidv4()}`;
+    loggedInUsers.push({
+      user: userName,
+      auth_token
+    });
+    return auth_token;
+  } else {
+    return user[0].auth_token;
+  }
+}
+
+// -----------------------------------------------------
+function removeLoggedInUser(auth_token) {
+  const user = loggedInUsers.filter(u => u.auth_token === auth_token);
+  if (!user || user.length === 0) {
+    return false;
+  }
+
+  loggedInUsers = loggedInUsers.filter(u => u.auth_token !== auth_token);
+  return true;
+}
+
+// -----------------------------------------------------
+function getLoggedInUser(ctx) {
+  if (!ctx.request.header.authorization) {
+    if (AUTH_ENABLED) {
+      return null;
+    } else {
+      return {
+        user: "UNKNOWN_USER",
+        auth_token: null
+      }
+    }
+  }
+
+  const auth_token = ctx.request.header.authorization;
+  const user = loggedInUsers.filter(u => u.auth_token === auth_token);
+  if (!user || user.length === 0) {
+    return null;
+  }
+
+  return user[0];  
+}
+
+// -----------------------------------------------------
+function userNotLoggedIn(ctx) {
+  ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+  ctx.status = 401;  
+}
+
+// -----------------------------------------------------
+router.post('/logout', async ctx => {
+  if (ctx.request.header.authorization) {
+    const result = removeLoggedInUser(ctx.request.header.authorization);
+    if (result) {
+      ctx.status = 200;
+    } else {
+      ctx.status = 404;
+    }
+  }
+
+  ctx.status = 400;
+});
+
+// -----------------------------------------------------
+router.post('/auth', KoaBody(), async ctx => {
+  const body = ctx.request.body;
+  const user = body.user;
+  const password = body.password;
+
+  const folderPath = path.join(STORAGE_PATH, "auth");
+  const userFile = path.join(folderPath, user + ".json");
+  console.log("trying to read " + userFile);
+  ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+
+  if (!fs.existsSync(userFile)) {
+    ctx.response.status = 401;
+    ctx.body = {
+      auth: false,
+      message: "User not found",
+      auth_token: null
+    }
+
+    return;
+  }
+
+  const contents = await fsp.readFile(userFile, 'utf-8');
+  const json = JSON.parse(contents);
+  if (json.password === password) {
+    ctx.response.status = 200;
+    ctx.body = {
+      auth: true,
+      message: "Logged in",
+      auth_token: addLoggedInUser(user)
+    }
+
+    return;
+  }
+
+  ctx.response.status = 401;
+  ctx.body = {
+    auth: false,
+    message: "Invalid password",
+    auth_token: null
+  };
+});
+
+// -------------------------------------------------
+router.get('/todos', async ctx => {
+  const user = getLoggedInUser(ctx);
+  if (!user) {
+    return userNotLoggedIn(ctx);
+  }
+
+  const fullPath = path.join(STORAGE_PATH, "todos", user.user + '.json');
+
+  // if the given file doesn't exist, return 404
+  if (!fs.existsSync(fullPath))
+  {
+    ctx.res.setHeader("Content-Type", "application/json");
+    ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+      ctx.body = [];
+    ctx.status = 200;
+    return;
+  }
+
+  const contents = await fsp.readFile(fullPath, 'utf-8');
+  const json = JSON.parse(contents);
+  ctx.body = json;
+  ctx.res.setHeader("Content-Type", "application/json");
+  ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+  ctx.response.status = 200;
+  console.log(`${APP_NAME}: Requested todos for ${user.user}, returned: ${fullPath}`)
+}); 
+
+// -------------------------------------------------
+router.post('/todos', KoaBody(), async ctx => {
+  const user = getLoggedInUser(ctx);
+  if (!user) {
+    return userNotLoggedIn(ctx);
+  }
+
+  const fullPath = path.join(STORAGE_PATH, "todos", user.user + '.json');
+  const contents = JSON.stringify(ctx.request.body);
+
+  await fsp.writeFile(fullPath, contents);
+  ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+  ctx.response.status = 200;
+  console.log(`${APP_NAME}: Saved todos for ${user.user} to ${fullPath}`);
+});
+
 
 // -------------------------------------------------
 router.get('/', ctx => {
@@ -41,6 +202,10 @@ router.get('/:folder', async ctx => {
   const folder = ctx.params.folder;
   const fullPath = path.join(STORAGE_PATH, folder);
 
+  if (!getLoggedInUser(ctx)) {
+    return userNotLoggedIn(ctx);
+  }
+  
   // if the given path doesn't exist, return 404
   if (!fs.existsSync(fullPath))
   {
@@ -69,6 +234,10 @@ router.get('/:folder', async ctx => {
 // Get the JSON contents of a particular file in our
 // storage folder.
 router.get('/:folder/:file', async ctx => {
+  if (!getLoggedInUser(ctx)) {
+    return userNotLoggedIn(ctx);
+  }
+
   const folder = ctx.params.folder;
   const file = ctx.params.file;
   const fullPath = path.join(STORAGE_PATH, folder, file + '.json');
@@ -93,6 +262,10 @@ router.get('/:folder/:file', async ctx => {
 // Save JSON data out to a file under the given folder,
 // using the folder as its name
 router.post('/:folder/:file', KoaBody(), async ctx => {
+  if (!getLoggedInUser(ctx)) {
+    return userNotLoggedIn(ctx);
+  }
+
   const folder = ctx.params.folder;
   const file = ctx.params.file;
   const folderPath = path.join(STORAGE_PATH, folder);
@@ -107,7 +280,33 @@ router.post('/:folder/:file', KoaBody(), async ctx => {
   ctx.res.setHeader("Access-Control-Allow-Origin", "*");
   ctx.response.status = 200;
   console.log(`${APP_NAME}: Saved contents for ${folder}/${file} to ${fullPath}`);
-})
+});
+
+// -------------------------------------------------
+router.del('/:folder/:file', async ctx => {
+  if (!getLoggedInUser(ctx)) {
+    return userNotLoggedIn(ctx);
+  }
+
+  const folder = ctx.params.folder;
+  const file = ctx.params.file;
+  const folderPath = path.join(STORAGE_PATH, folder);
+  const fullPath = path.join(STORAGE_PATH, folder, file + '.json');
+
+  if (!fs.existsSync(folderPath)) {
+    ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+    ctx.response.status = 200;
+    console.log(`${APP_NAME}: Delete ${folder}/${file} (did not exist)`);
+  }
+
+  await fsp.unlink(fullPath);
+
+  ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+  ctx.response.status = 200;
+  console.log(`${APP_NAME}: Delete ${folder}/${file} from ${fullPath}`);
+
+});
+
 
 app
   .use(KoaCors())
